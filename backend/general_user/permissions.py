a6da78ca1e02343e3629849ab5d3a77df8467e3e
mhnasajpour <mhnasajpour@gmail.com>
2022-08-11 23:59:43 +0430

    [general_user] write permissions

--- backend/general_user/permissions.py
+++ backend/general_user/permissions.py
@@ -1,9 +1,25 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from config.settings import GENERAL_USER, BUSINESS_USER
+
+
+class IsQualified(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.first_name)
+
+
+class IsGeneralUser(BasePermission):
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated and request.user.type == GENERAL_USER)
+
+
+class IsPublicPlace(BasePermission):
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated and request.user.type == BUSINESS_USER)
 
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
-            request.method in SAFE_METHODS or
-            request.user.is_authenticated and request.user.is_staff
+            (request.method in SAFE_METHODS) or
+            (request.user.is_authenticated and request.user.is_staff and request.user.first_name)
         )
