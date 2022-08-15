544768999fee88057d4240d03463ad1a1b84577d
mhnasajpour <mhnasajpour@gmail.com>
2022-08-15 16:33:16 +0430

    [general_user][views] add two func update_status and meetings_people and implement ListCreateMeetPeopleView

--- backend/general_user/views.py
+++ backend/general_user/views.py
@@ -1,13 +1,48 @@
-from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from .serializers import GeneralUserSerializer, RecordLatestHealthStatusSerializer, ListUserStatusSerializer
+from .serializers import GeneralUserSerializer, RecordLatestHealthStatusSerializer, ListUserStatusSerializer, ListCreateMeetPeopleserializers
 from public_place.serializers import PublicPlaceSerializer
 from rest_framework.views import APIView
 from .permissions import IsQualified, IsGeneralUser
 from rest_framework.response import Response
 from rest_framework import status
-from .models import UserStatus
+from .models import GeneralUser, UserStatus, MeetPeople
 from datetime import datetime, timedelta
+from django.db.models import Q
+from public_place.views import meetings_place
+from django.db import transaction
+
+
+def update_status(user):
+    user_status = user.userstatus_set.last()
+    level = (datetime.now().date() - user_status.date_created.date()).days // 7
+
+    if 1 < user_status.status < 4 and level:
+        new_status = user_status.status-level
+        UserStatus.objects.create(
+            user=user, effective_factor=-1, status=new_status if new_status > 0 else 1)
+
+
+def meetings_people(user):
+    update_status(user)
+
+    if user.status == 4:
+        meetings_place(user)
+
+    if user.status > 2:
+        persons1 = list(user.meet_people1
+                        .filter(date_created__gt=datetime.now()-timedelta(days=7))
+                        .values_list('user2', flat=True))
+
+        persons2 = list(user.meet_people2
+                        .filter(date_created__gt=datetime.now()-timedelta(days=7))
+                        .values_list('user1', flat=True))
+
+        for person in persons1 + persons2:
+            if GeneralUser.objects.get(pk=person).status < user.status:
+                UserStatus.objects.create(
+                    user_id=person, effective_factor=user.pk, status=user.status-1)
+                meetings_people(GeneralUser.objects.get(pk=person))
 
 
 class UserDetailsView(RetrieveUpdateAPIView):
@@ -31,6 +66,11 @@ class RecordLatestHealthStatusView(APIView):
     serializer_class = RecordLatestHealthStatusSerializer
 
     def post(self, request):
+        last_status = request.user.generaluser.userstatus_set.last()
+        if last_status and not last_status.effective_factor and not datetime.now().date() - last_status.date_created.date():
+            return Response({'message': 'You can only test once a day.'},
+                            status=status.HTTP_403_FORBIDDEN)
+
         serializer = RecordLatestHealthStatusSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
@@ -49,9 +89,12 @@ class RecordLatestHealthStatusView(APIView):
         else:
             health_status = 1
 
-        if request.user.generaluser.status <= health_status:
-            UserStatus.objects.create(
-                user=request.user.generaluser, status=health_status)
+        with transaction.atomic():
+            if request.user.generaluser.status <= health_status:
+                UserStatus.objects.create(
+                    user=request.user.generaluser, effective_factor=0, status=health_status)
+                meetings_people(request.user.generaluser)
+                return Response({'status': request.user.generaluser.status}, status=status.HTTP_201_CREATED)
 
         return Response({'status': request.user.generaluser.status}, status=status.HTTP_200_OK)
 
@@ -62,5 +105,52 @@ class ListUserStatusView(ListAPIView):
 
     def get_queryset(self):
         return self.request.user.generaluser.userstatus_set \
-            .filter(date_created__gt=datetime.now()-timedelta(days=self.kwargs['day'])) \
-            .distinct('date_created__date', 'status')[::-1]
+            .filter(date_created__gt=datetime.now()-timedelta(days=self.kwargs['day']))[::-1]
+
+
+class ListCreateMeetPeopleView(ListCreateAPIView):
+    permission_classes = [IsQualified, IsGeneralUser]
+    serializer_class = ListCreateMeetPeopleserializers
+
+    def get_queryset(self):
+        user = self.request.user.generaluser
+        return MeetPeople.objects.filter(Q(user1=user) | Q(user2=user)) \
+            .filter(date_created__gt=datetime.now()-timedelta(days=self.request.GET.get('day', 1)))[::-1]
+
+    def post(self, request):
+        serializer = ListCreateMeetPeopleserializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user_target = GeneralUser.objects.get(user=serializer.data['user'])
+
+        if self.get_queryset():
+            meetings = [(obj.user1, obj.user2) for obj in self.get_queryset()]
+            if (request.user.generaluser, user_target) in meetings:
+                return Response({'message': 'You have already saved this appointment.'}, status=status.HTTP_200_OK)
+            if (user_target, request.user.generaluser) in meetings:
+                return Response({'message': 'This meeting has already been saved.'}, status=status.HTTP_200_OK)
+
+        with transaction.atomic():
+            meet_people = MeetPeople()
+            meet_people.user1 = request.user.generaluser
+            meet_people.user2 = user_target
+            meet_people.save()
+
+            # import pdb
+            # pdb.set_trace()
+            status1 = request.user.generaluser.status
+            status2 = user_target.status
+            if (status1 > 2 or status2 > 2) and status1 != status2:
+                if status1 > status2:
+                    user = user_target
+                    factor = request.user.generaluser.pk
+                    new_status = status1 - 1
+                else:
+                    user = request.user.generaluser
+                    factor = serializer.data['user']
+                    new_status = status2 - 1
+
+                UserStatus.objects.create(
+                    user=user, effective_factor=factor, status=new_status)
+                meetings_people(user)
+
+            return Response({'status': request.user.generaluser.status}, status=status.HTTP_201_CREATED)
