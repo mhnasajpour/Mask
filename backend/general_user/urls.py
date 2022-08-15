64f77ed0cc8dc874d1edd3bdc968848468f3d95f
mhnasajpour <mhnasajpour@gmail.com>
2022-08-15 16:30:39 +0430

    [general_user][urls] add status and meet person

--- backend/general_user/urls.py
+++ backend/general_user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserDetailsView, RecordLatestHealthStatusView, ListUserStatusView
+from .views import UserDetailsView, RecordLatestHealthStatusView, ListUserStatusView, ListCreateMeetPeopleView
 
 urlpatterns = [
     path('', UserDetailsView.as_view(), name='user'),
     path('test/', RecordLatestHealthStatusView.as_view(), name='test'),
-    path('status/<int:day>', ListUserStatusView.as_view(), name='status'),
+    path('status/<int:day>/', ListUserStatusView.as_view(), name='status'),
+    path('meet/person/', ListCreateMeetPeopleView.as_view(), name='meet_person'),
 ]
