c5650d6402a18674481deb7c111b5210596ab3c2
mhnasajpour <mhnasajpour@gmail.com>
2022-08-16 16:39:07 +0430

    [public_place][urls] implement ListCreateMeetPlace

--- backend/public_place/serializers.py
+++ backend/public_place/serializers.py
@@ -1,25 +1,6 @@
 from rest_framework import serializers
-from .models import PublicPlace, PLACE_STATUS_CHOICES
-from general_user.serializers import AbstractUserDetailsSerializer
-
-
-class PublicPlaceSerializer(AbstractUserDetailsSerializer):
-    name = serializers.CharField(max_length=255, required=True)
-    location = serializers.CharField(max_length=255, required=True)
-    region = serializers.IntegerField(min_value=0, required=True)
-
-    class Meta:
-        model = PublicPlace
-        fields = ('pk', 'type', 'is_staff', 'status', 'first_name', 'last_name', 'username', 'email',
-                  'national_code', 'phone_number', 'name', 'location', 'region')
-
-    def update(self, instance, data):
-        self.update_user(instance=instance.user, data=data['user'])
-        instance.name = data.get('name', instance.name)
-        instance.location = data.get('location', instance.location)
-        instance.region = data.get('region', instance.region)
-        instance.save()
-        return instance
+from .models import PublicPlace, MeetPlace, WHITEPLACE, PLACE_STATUS_CHOICES
+from datetime import timedelta
 
 
 class ChangePlaceStatusSerializer(serializers.Serializer):
@@ -32,3 +13,32 @@ class MinorPlaceDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = PublicPlace
         fields = ('pk', 'status', 'email', 'name', 'location', 'region')
+
+
+class ListCreateMeetPlaceSerializer(serializers.ModelSerializer):
+    status_user = serializers.SerializerMethodField(read_only=True)
+    status_place = serializers.SerializerMethodField(read_only=True)
+    date = serializers.DateTimeField(
+        source='date_created', format='%Y-%m-%d', read_only=True)
+
+    class Meta:
+        model = MeetPlace
+        fields = ('place', 'status_user', 'status_place', 'date')
+
+    def get_status_user(self, obj):
+        try:
+            return obj.user.userstatus_set \
+                .filter(date_created__gte=obj.date_created,
+                        date_created__lte=obj.date_created + timedelta(days=7)) \
+                .last().status
+        except:
+            return 1
+
+    def get_status_place(self, obj):
+        try:
+            return obj.place.placestatus_set \
+                .filter(date_created__gte=obj.date_created,
+                        date_created__lte=obj.date_created + timedelta(days=7)) \
+                .last().status
+        except:
+            return WHITEPLACE
