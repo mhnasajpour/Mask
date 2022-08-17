e808f9fb3f80fc8b326d008b1258de54789b5481
mhnasajpour <mhnasajpour@gmail.com>
2022-08-17 22:17:25 +0430

    [hospital][urls] change pk with zip code

--- backend/hospital/urls.py
+++ backend/hospital/urls.py
@@ -3,5 +3,5 @@ from .views import ListCreateHospitalView, RetrieveUpdateDestroyHospitalView
 
 urlpatterns = [
     path('', ListCreateHospitalView.as_view()),
-    path('<int:pk>', RetrieveUpdateDestroyHospitalView.as_view()),
+    path('<int:zip_code>', RetrieveUpdateDestroyHospitalView.as_view()),
 ]
