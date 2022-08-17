7c65855b9d61488964d8e4008bf8ba31300edd01
mhnasajpour <mhnasajpour@gmail.com>
2022-08-17 22:15:22 +0430

    [hospital][views] implement get_queryset for ListCreateHospital

--- backend/hospital/views.py
+++ backend/hospital/views.py
@@ -7,10 +7,25 @@ from .models import Hospital
 class ListCreateHospitalView(ListCreateAPIView):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = HospitalSerializer
-    queryset = Hospital.objects.all()
+
+    def get_queryset(self):
+        city = self.request.GET.get('city')
+        if city:
+            queryset = Hospital.objects.filter(city=city)
+        else:
+            queryset = Hospital.objects.all()
+
+        latitude = float(self.request.GET.get('lat'))
+        longitude = float(self.request.GET.get('lon'))
+        if latitude and longitude:
+            queryset = list(queryset)
+            queryset.sort(
+                key=lambda x: (x.latitude-latitude)**2 + (x.longitude-longitude)**2)
+        return queryset
 
 
 class RetrieveUpdateDestroyHospitalView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = HospitalSerializer
     queryset = Hospital.objects.all()
+    lookup_field = 'zip_code'
