5b76e0af0faefa6e2ee6f91e494fbf9bc9772763
mhnasajpour <mhnasajpour@gmail.com>
2022-08-17 22:18:05 +0430

    [hospital][admin] implement PlaceAdmin

--- backend/hospital/admin.py
+++ backend/hospital/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Hospital
 
-# Register your models here.
+
+@admin.register(Hospital)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'zip_code', 'address')
+    readonly_fields = ('pk', 'map')
+
+    fieldsets = (
+        ('Specifications', {
+            'fields': ('pk', 'name', 'city', 'zip_code')
+        }),
+        ('Address', {
+            'fields': ('address', ('latitude', 'longitude'), 'map'),
+        }),
+    )
+
+    def map(self, obj):
+        return format_html(f'<iframe src="https://maps.google.com/maps?q={obj.latitude},{obj.longitude}&hl=en&z=16&amp;output=embed" width="483" height="300"></iframe>')
