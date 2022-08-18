e4de5b5f9160a2bc99a9f292ea754b0bae6f26cf
mhnasajpour <mhnasajpour@gmail.com>
2022-08-18 13:44:19 +0430

    [hospital][serializers] make fields required in Hospital

--- backend/hospital/serializers.py
+++ backend/hospital/serializers.py
@@ -3,10 +3,17 @@ from .models import Hospital
 
 
 class HospitalSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=150)
+    city = serializers.CharField(max_length=150)
+    zip_code = serializers.CharField(max_length=10)
+    address = serializers.CharField(max_length=500)
+    longitude = serializers.FloatField()
+    latitude = serializers.FloatField()
+
     class Meta:
         model = Hospital
         fields = ('name', 'city', 'zip_code',
-                  'address', 'longitude', 'latitude')
+                  'address', 'latitude', 'longitude')
 
     def validate_zip_code(self, zip_code):
         if len(zip_code) == 10 and zip_code.isnumeric():
