3e68efb14fd8475afd97cc31d8a4d16a0b32817c
mhnasajpour <mhnasajpour@gmail.com>
2022-08-20 10:48:38 +0430

    [public_place][admin] implement BusinessOwnerAdmin

--- backend/public_place/admin.py
+++ backend/public_place/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, BusinessOwner, PlaceStatus, MeetPlace
+from django_reverse_admin import ReverseModelAdmin
 
 
 @admin.register(Place)
@@ -18,3 +19,52 @@ class PlaceAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return False
+
+
+@admin.register(BusinessOwner)
+class BusinessOwnerAdmin(ReverseModelAdmin):
+    inline_type = 'stacked'
+    inline_reverse = (
+        ('user', {'fields': (('first_name', 'last_name'), ('username', 'password'),
+                             'email', 'national_code', 'phone_number', 'is_staff', 'is_active')}),
+        ('place', {'fields': ('name', 'city', 'zip_code',
+                              'address', ('latitude', 'longitude'))}),
+    )
+
+    list_display = ('pk', 'name', 'zip_code',
+                    'first_name', 'last_name', 'email')
+
+    fields = ('pk', 'user_id', 'place_id', 'map')
+    readonly_fields = fields
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        obj.user.set_password(obj.user.password)
+        obj.user.save()
+
+    def has_add_permission(self, request):
+        return False
+
+    def name(self, obj):
+        return obj.place.name
+
+    def zip_code(self, obj):
+        return obj.place.zip_code
+
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    def last_name(self, obj):
+        return obj.user.last_name
+
+    def email(self, obj):
+        return obj.user.email
+
+    def user_id(self, obj):
+        return obj.user.pk
+
+    def place_id(self, obj):
+        return obj.place.pk
+
+    def map(self, obj):
+        return obj.place.map
