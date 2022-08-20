0bf7e0ec5f5e55bbd860197c07a7a35dfb51c251
mhnasajpour <mhnasajpour@gmail.com>
2022-08-20 23:26:08 +0430

    [general_user][admin] implement GeneralUserAdmin and StatusFilter

--- backend/general_user/admin.py
+++ backend/general_user/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import User, GeneralUser, UserStatus, MeetPeople, GENERAL_USER, BUSINESS_USER
+from django.utils.html import format_html
 
 
 class TypeFilter(admin.SimpleListFilter):
@@ -58,3 +59,64 @@ class UserAdmin(admin.ModelAdmin):
             return 'General user'
         if hasattr(obj, 'businessowner'):
             return 'Business owner'
+
+
+class StatusFilter(admin.SimpleListFilter):
+    title = 'status'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            (1, 'Healthy'),
+            (2, 'Doubtful'),
+            (3, 'Perilous'),
+            (4, 'Patient'),
+            (5, 'Dead')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            pks = [obj.pk for obj in queryset if obj.status ==
+                   int(self.value())]
+            return queryset.filter(pk__in=pks)
+        else:
+            return queryset
+
+
+@admin.register(GeneralUser)
+class GeneralUserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'email', 'status')
+    readonly_fields = ('user', 'first_name', 'last_name', 'email', 'status')
+    fieldsets = (
+        ('User', {
+            'fields': ('status', 'user', 'first_name', 'last_name', 'email')
+        }),
+        ('Others', {
+            'fields': ('birth_date', 'blood_type', 'height', 'weight')
+        })
+    )
+    list_filter = (StatusFilter, 'date_created')
+    search_fields = ('user__username', 'user__email', 'user__first_name',
+                     'user__last_name', 'user__national_code', 'user__phone_number')
+
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    def last_name(self, obj):
+        return obj.user.last_name
+
+    def email(self, obj):
+        return obj.user.email
+
+    def status(self, obj):
+        if obj.status == 1:
+            color = '60,179,113'
+        if obj.status == 2:
+            color = '255,180,51'
+        if obj.status == 3:
+            color = '225,52,45'
+        if obj.status == 4:
+            color = '142,15,6'
+        if obj.status == 5:
+            color = '50,50,50'
+        return format_html(f'<p style="background-color: rgb({color}); color: white; padding: 1px 3px; width: 30px; border-radius: 10px; text-align: center; margin: 0px; font-size: 11px;">{obj.status}</p>')
