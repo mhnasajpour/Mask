2afdc016dcccda6060ea207abd10bceee3682d4f
mhnasajpour <mhnasajpour@gmail.com>
2022-08-20 10:47:48 +0430

    [public_place][admin] implement place

--- backend/public_place/admin.py
+++ backend/public_place/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Place
 
-# Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'zip_code', 'address')
+    readonly_fields = ('pk', 'map')
+    list_filter = ('city',)
+    fieldsets = (
+        ('Specifications', {
+            'fields': ('pk', 'name', 'city', 'zip_code')
+        }),
+        ('Address', {
+            'fields': ('address', ('latitude', 'longitude'), 'map'),
+        }),
+    )
+
+    def has_add_permission(self, request):
+        return False
