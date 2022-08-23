c259bd2b35b10d39e587b5f0e3d3d8a610d0830f
mhnasajpour <mhnasajpour@gmail.com>
2022-08-23 11:26:05 +0430

    [general_user][tasks] add places to update_status

--- backend/general_user/tasks.py
+++ backend/general_user/tasks.py
@@ -1,18 +1,23 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from .models import GeneralUser, UserStatus
+from public_place.models import BusinessOwner, PlaceStatus
+from config.settings import WHITEPLACE, REDPLACE
 
 
 def update_status():
-    for user in GeneralUser.objects.all():
+    for user in [obj for obj in GeneralUser.objects.all() if 1 < obj.status < 4]:
         user_status = user.userstatus_set.last()
         level = (datetime.now().date() -
                  user_status.date_created.date()).days // 7
-
-        if 1 < user_status.status < 4 and level:
+        if level:
             new_status = user_status.status-level
             UserStatus.objects.create(
                 type=4, user=user, status=new_status if new_status > 0 else 1)
+
+    for place in [obj for obj in BusinessOwner.objects.all() if obj.status == REDPLACE]:
+        if (datetime.now().date() - place.placestatus_set.last().date_created.date()).days >= 14:
+            PlaceStatus.objects.create(type=3, place=place, status=WHITEPLACE)
 
 
 scheduler = BackgroundScheduler()
