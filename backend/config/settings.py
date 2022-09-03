b205d809653c1fffe7aed89551c4fc862344f273
mhnasajpour <mhnasajpour@gmail.com>
2022-09-03 15:29:26 +0430

    [config][settings] fix allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from datetime import timedelta
 from pathlib import Path
 import environ
 
@@ -30,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
